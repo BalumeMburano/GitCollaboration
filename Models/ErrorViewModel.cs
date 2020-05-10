d3eab9012a5c458a3939ae6c69d263d27b2bcb40
BalumeMburano <45223634+BalumeMburano@users.noreply.github.com>
2020-05-10 15:20:22 +1000

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GitCollaboration.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
